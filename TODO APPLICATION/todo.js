bbfedf63f6190c50cff9515589578adc1c768918
phraew6334100241 <76487352+phraew6334100241@users.noreply.github.com>
2022-07-24 16:47:32 +0700

    TODO APPLICATION

---
+++ TODO APPLICATION/todo.js
@@ -0,0 +1,27 @@
+const inputText = document.querySelector('#text');
+const myButton = document.querySelector('.btn-list');
+const list = document.querySelector('.container ul');
+myButton.addEventListener('click',(e)=>{
+    if(inputText.value !=""){
+        e.preventDefault();
+        //li
+        const myLi = document.createElement('li');
+        myLi.innerHTML = inputText.value;
+        list.appendChild(myLi);
+        //span
+        const mySpan = document.createElement('span');
+        mySpan.innerHTML = 'X';
+        myLi.appendChild(mySpan);
+    }
+    const close = document.querySelectorAll('span');
+    for(let i=0;i<close.length;i++){
+        close[i].addEventListener('click',()=>{
+            close[i].parentElement.style.opacity = 0;
+            setTimeout(()=>{
+                close[i].parentElement.style.display="none";
+            },500);
+            
+        })
+    }
+    inputText.value="";
+});
